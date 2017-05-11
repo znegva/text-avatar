838067f716d032718c8f68c675f1b8a889904299
Martin Kausche <mail@martinkausche.de>
2017-05-12 00:25:02 +0200

    chg: modified example-project to show ion-avatar next to text-avatar (also needed to update ionic since $ion-xxx-avatar-border-radius was introduced in april...)

--- example-app/src/components/text-avatar/text-avatar.ts
+++ example-app/src/components/text-avatar/text-avatar.ts
@@ -1,18 +1,32 @@
-import { Directive, ElementRef, Input } from '@angular/core';
-import { ColorGenerator } from "./color-generator";
+import {Directive, ElementRef, Input, SimpleChanges} from '@angular/core';
+import {ColorGenerator} from "./color-generator";
 
+/**
+ * Generated class for the TextAvatar directive.
+ *
+ * See https://angular.io/docs/ts/latest/api/core/index/DirectiveMetadata-class.html
+ * for more info on Angular Directives.
+ */
 @Directive({
-  selector: 'text-avatar',
-  providers: [ColorGenerator]
+	selector: 'text-avatar',
+	providers: [ColorGenerator]
 })
 export class TextAvatar {
 
-  constructor(private element: ElementRef, private colorGenerator: ColorGenerator) { }
+	constructor(private element: ElementRef, private colorGenerator: ColorGenerator){  }
 
-  @Input()
-  set text(txt: string) {
-    this.element.nativeElement.setAttribute("value", txt.charAt(0));
-    this.element.nativeElement.style.backgroundColor = this.colorGenerator.getColor(txt);
-  }
-
+	@Input() text: string;
+	@Input() color: string;
+	ngOnChanges(changes: SimpleChanges){
+		let text = changes['text'] ? changes['text'].currentValue : '';
+		let color = changes['color'] ? changes['color'].currentValue : '';
+		if(text){
+			this.element.nativeElement.setAttribute("value", text.charAt(0));
+			if(color){
+				this.element.nativeElement.style.backgroundColor = color;
+			}else{
+				this.element.nativeElement.style.backgroundColor = this.colorGenerator.getColor(text);
+			}
+		}
+	}
 }
